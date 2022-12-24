6ba0128609183112c042a0e01bf6c1bb292453cb
Moh Achun Armando <mohamad.armando@kumparan.com>
2022-12-24 23:49:33 +0700

    feat: add toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import Feeds from "./components/feeds"
 import Header from "./components/header"
 import NavBar from "./components/navbar"
 import { threads } from "./dummy/dummy"
+import { ToastContainer } from "react-toastify"
+import { injectStyle } from "react-toastify/dist/inject-style"
 
 function App() {
+  injectStyle()
   return (
     <div className="App relative">
       <Header />
       <Feeds threads={threads} />
       <NavBar />
+      <ToastContainer limit={1} />
     </div>
   )
 }
--- src/components/thread.tsx
+++ src/components/thread.tsx
@@ -1,5 +1,6 @@
 import { Thread } from "../types/model"
 import communityLogo from "../assets/community-logo.svg"
+import { toast, ToastContent } from "react-toastify"
 
 const ThreadComponent = (props: { thread: Thread }) => {
   let leadMedia
@@ -12,6 +13,30 @@ const ThreadComponent = (props: { thread: Thread }) => {
       />
     )
   }
+
+  let content = "isi komentar ini sangat panjang sekali"
+  let subject = props.thread.created_by.username
+
+  const msg = () => (
+    <div className="text-left text-sm text-black">
+      <div className="font-bold">{props.thread.title}</div>
+      <div>
+        <span className="font-bold">{subject ? subject + ": " : ""}</span>"
+        {subject.length + content.length > 35
+          ? content.substring(0, 35 - subject.length) + "..."
+          : content}
+        "
+      </div>
+    </div>
+  )
+
+  const notify = () =>
+    toast(msg, {
+      position: "top-center",
+      hideProgressBar: true,
+      draggable: true,
+      pauseOnHover: true,
+    })
 
   return (
     <div className="Thread w-full border-b-2   leading-5">
@@ -28,8 +53,10 @@ const ThreadComponent = (props: { thread: Thread }) => {
             </div>
           </div>
         </div>
-        <div className="text-left">
-          <div className="mb-1 text-lg font-bold">{props.thread.title}</div>
+        <div className="text-left" onClick={notify}>
+          <div className="mb-1 text-lg font-bold hover:cursor-pointer">
+            {props.thread.title}
+          </div>
           <div className="text-justify">{props.thread.content}</div>
         </div>
       </div>
